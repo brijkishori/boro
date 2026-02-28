fbb838df02319fbf9e9a24e4bb6b46da2f2ad4b4
brijkishori <birju.khare@gmail.com>
2026-02-28 15:37:57 -0500

    updated menifest file to have only 5 tags i had about 7

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -24,7 +24,7 @@ export async function GET() {
         `${appUrl}/screenshots/s3.png`  // Ensure you create this file in /public/screenshots/
       ],
       primaryCategory: "finance",
-      tags: ["defi", "lending", "bitcoin", "base","morpho","usdc","borrow"], // <-- ADDED HERE
+      tags: ["lending", "bitcoin", "base","morpho","borrow"], // <-- ADDED HERE
       heroImageUrl: `${appUrl}/hero.png`, // The discovery feed cover photo
       tagline: "Unlock BTC liquidity instantly",
       ogTitle: "Simple BTC Borrow",
