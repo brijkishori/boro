79e6dbaa7d986decb4db08553baab2dc0c08497d
brijkishori <birju.khare@gmail.com>
2026-02-28 13:42:00 -0500

    updated accountAssociatio details in route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -4,11 +4,11 @@ export async function GET() {
   const appUrl = 'https://boro-ruddy.vercel.app'; // Your live Vercel domain
 
   const config = {
-    accountAssociation: {
-      header: "PASTE_YOUR_HEADER_HERE",
-      payload: "PASTE_YOUR_PAYLOAD_HERE",
-      signature: "PASTE_YOUR_SIGNATURE_HERE"
-    },
+    "accountAssociation": {
+    "header": "eyJmaWQiOjI4NDAyMTUsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhhQThDODQ5MUNmMTFhOEQwRUJkRjYxNDAwMGU0MmQ0MzRiZUQ2RmM1In0",
+    "payload": "eyJkb21haW4iOiJib3JvLXJ1ZGR5LnZlcmNlbC5hcHAifQ",
+    "signature": "L/+mgYlAuiiZzrHAl2kTsjO6gA1MKTFXCTDhhqk8BRxIPsnk2VqrOYyUL5NShrPlPrgN/vGbCDhgggnZy8sPPxw="
+  },
     frame: {
       version: "1",
       name: "BORO",
@@ -22,4 +22,13 @@ export async function GET() {
   };
 
   return NextResponse.json(config);
+}
+
+
+{
+  "accountAssociation": {
+    "header": "eyJmaWQiOjI4NDAyMTUsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhhQThDODQ5MUNmMTFhOEQwRUJkRjYxNDAwMGU0MmQ0MzRiZUQ2RmM1In0",
+    "payload": "eyJkb21haW4iOiJib3JvLXJ1ZGR5LnZlcmNlbC5hcHAifQ",
+    "signature": "L/+mgYlAuiiZzrHAl2kTsjO6gA1MKTFXCTDhhqk8BRxIPsnk2VqrOYyUL5NShrPlPrgN/vGbCDhgggnZy8sPPxw="
+  }
 }
